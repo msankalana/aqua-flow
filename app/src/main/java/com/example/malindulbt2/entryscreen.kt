8ac44651696d505131b2c7003bacc41d54434877
Danindu Chamoth <154650297+danindu-chamoth@users.noreply.github.com>
2025-08-25 18:39:38 +0530

    fix erro

--- app/src/main/java/com/example/malindulbt2/entryscreen.kt
+++ app/src/main/java/com/example/malindulbt2/entryscreen.kt
@@ -4,28 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-class entryscreen {
 
+class entryscreen : AppCompatActivity() {
 
-    class WelcomeActivity:  AppCompatActivity() {
+    private lateinit var loginBtn: Button
 
-        private lateinit var signupBtn: Button
-        private lateinit var loginBtn: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.entryscreen)
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.entryscreen) // use your welcome XML
+        // Find buttons
+        loginBtn = findViewById(R.id.login_btn2)
 
-            // Find buttons
-
-            loginBtn = findViewById(R.id.login_btn2)
-
-
-            // Navigate to LoginActivity
-            loginBtn.setOnClickListener {
-                val intent = Intent(this, loginscreen::class.java)
-                startActivity(intent)
-            }
+        // Navigate to LoginActivity
+        loginBtn.setOnClickListener {
+            val intent = Intent(this, loginscreen::class.java)
+            startActivity(intent)
         }
     }
 }
